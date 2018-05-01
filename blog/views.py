8e4f8575676ad4e768e2cd4838e1757a9c276da0
tokyoyo <forsberg_johanna@homtail.com>
2018-05-01 15:23:24 +0200

    tried to fix the difference with local blog

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request,'blog/post_list.html',{'posts':posts})
+	return render(request, 'blog/post_list.html',{'posts':posts})
 
 
 
